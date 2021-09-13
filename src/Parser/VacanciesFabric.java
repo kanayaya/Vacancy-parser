fc743ef2997445e3e4360ff737228f2c8ae8f1ea
Kanaya <kanayaya@mail.ru>
2021-09-13 19:33:02 +0800

    Now you can add new site just with making its fabric, block and view))

---
+++ src/Parser/VacanciesFabric.java
@@ -0,0 +1,19 @@
+package Parser;
+
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+
+public abstract class VacanciesFabric {
+    private ArrayList<VacancyBlock> vacancies;
+
+    public VacanciesFabric(String link) throws InterruptedException {
+        this.vacancies = getVacanciesFrom(Picker.repeatedlyGetPage(link));
+    }
+
+    public ArrayList<VacancyBlock> getVacancies() {
+        return vacancies;
+    }
+
+    public abstract ArrayList<VacancyBlock> getVacanciesFrom(Document wholePage);
+}
